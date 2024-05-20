dc47f9deefe0b1468e468a557d020e06623138f5
chris <christiem802@gmail.com>
2024-05-20 11:12:32 +0530

    Completed the creation of the ALU unit

---
+++ Register/register_block.v
@@ -0,0 +1,9 @@
+// the register block will be a 32 registers each 32 bits wide
+
+// to address all the blocks in 32 registers, we need to have 4 bits for addressing the register block
+module register_block(
+    input read_reg_data_1,
+    input read_reg_data_2
+)
+
+endmodule
